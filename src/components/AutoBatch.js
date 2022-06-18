22e2dd7b9b813b3531cb49f0538485c6332cf08d
code-yy <jiyeyouren523@gmail.com>
2022-06-18 12:16:52 +0900

    🎉 Begin a Project

---
+++ src/components/AutoBatch.js
@@ -0,0 +1,9 @@
+import { Layout } from "./Layout";
+
+export const AutoBatch = () => {
+  return (
+    <Layout>
+      <p className="my-3 text-xl font-bold text-blue-500">Automatic batching</p>
+    </Layout>
+  );
+};
